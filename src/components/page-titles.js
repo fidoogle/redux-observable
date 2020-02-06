5464d8ecf86c0ec278ac4356f1d47107bf68aac8
Fidel Guajardo <figuajardo@saws.org>
2020-02-06 10:57:55 -0600

    add simple login form

--- src/components/page-titles.js
+++ src/components/page-titles.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {sendLogin} from '../services/accounts'
+
 
 const PageTitle = () => {
+    const [email, setEmail] = useState('');
+    const [resultData, setResultData] = useState(null);
+    const [loginError, setLoginError] = useState(null);
+
+    const changeHandler = (event) => {
+        setEmail(event.target.value);
+    }
+    const login = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+        sendLogin(email).then(
+            p => {setResultData(p); console.log({resultData})}, 
+            e => {setLoginError(e); console.error({e})}
+        )
+    }
+
     return (
         <div className="page-titles">
             <div className="content-max">
@@ -9,6 +27,13 @@ const PageTitle = () => {
                     <div className="subtitle">
                         Expand and see more information about an account below.
                     </div>
+                </div>
+                <div>
+                    { loginError && 'There was an error' }
+                    <form>
+                        <input type="text" name="email" placeholder="email address" onChange={changeHandler}/>
+                        <button onClick={login}>Login</button>
+                    </form>
                 </div>
             </div>
         </div>
